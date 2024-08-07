74ffdd0a5bf9dcd59a6e00d6e18d2f747a61d6d5
haram0614 <139355979+haram0614@users.noreply.github.com>
2024-08-07 23:12:44 +0900

    Reset Test ....

--- js/layers.js
+++ js/layers.js
@@ -27,32 +27,3 @@ addLayer("q", {
     layerShown(){return true}
 })
 
-addLayer("T", {
-    name: "Test", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "T", // This appears on the layer's node. Default is the id with the first letter capitalized
-    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
-    startData() { return {
-        unlocked: true,
-		points: new Decimal(0),
-    }},
-    color: "#4BDC13",
-    requires: new Decimal("e40"), // Can be a function that takes requirement increases into account
-    resource: "Test", // Name of prestige currency
-    baseResource: "PL", // Name of resource prestige is based on
-    baseAmount() {return player.points}, // Get the current amount of baseResource
-    type: "static", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    exponent: 0.01, // Prestige currency exponent
-    branches: ["Q"]
-    gainMult() { // Calculate the multiplier for main currency from bonuses
-        mult = new Decimal(1)
-        return mult
-    },
-    gainExp() { // Calculate the exponent on main currency from bonuses
-        return new Decimal(1)
-    },
-    row: 1, // Row the layer is in on the tree (0 is the first row)
-    hotkeys: [
-        {key: "t", description: "T: Reset for Test", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
-    ],
-    layerShown(){return true}
-})
