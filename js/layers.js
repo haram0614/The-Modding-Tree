ce47a5caf8e527f66a46d41fd26d416e4c0fe5f9
haram0614 <139355979+haram0614@users.noreply.github.com>
2024-08-07 22:51:07 +0900

    test7-1

--- js/layers.js
+++ js/layers.js
@@ -30,7 +30,7 @@ addLayer("q", {
 addLayer("T", {
     name: "Test", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "T", // This appears on the layer's node. Default is the id with the first letter capitalized
-    position: 1, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
 		points: new Decimal(0),
