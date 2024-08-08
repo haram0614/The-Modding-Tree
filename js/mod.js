a9880cb24c2ba9eac9341f5ddba5e5f562964b32
haram0614 <139355979+haram0614@users.noreply.github.com>
2024-08-08 22:53:48 +0900

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -13,8 +13,8 @@ let modInfo = {
 
 // Set your version in num and name
 let VERSION = {
-	num: "0.0.6.5",
-	name: "Literally nothing.",
+	num: "0.0.6.6",
+	name: "Literally nothing21",
 }
 
 let changelog = `<h1>Changelog:</h1><br>
