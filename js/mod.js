ef1cd3188576e016a8325658410a738e697be236
haram0614 <139355979+haram0614@users.noreply.github.com>
2024-08-17 16:19:07 +0900

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -43,7 +43,7 @@ function getPointGen() {
 		return new Decimal(0)
 
 	let gain = new Decimal("1.8e43")
-	if (hasUpgrade("q", 13)) gain = gain.mul(player.points.pow(0.05))
+	if (hasUpgrade("q", 13)) gain = gain.mul(player.Quantum.pow(0.05))
 	return gain
 }
 
