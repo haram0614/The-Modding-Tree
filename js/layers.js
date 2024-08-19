58c1f3048d48ee46d95db0e46b0cf14fa37345aa
haram0614 <139355979+haram0614@users.noreply.github.com>
2024-08-19 22:37:02 +0900

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -116,7 +116,7 @@ addLayer("a", {
         cols: 6,
         11: {
             name: "INF",
-            tooltip: "Get 1.79e308 Quantum. Reward: 1 AP.",
+            tooltip: "Get 1.79e308 PL. Reward: 1 AP.",
             done() {
                 return player.points.gte(1.79e308)
             },
