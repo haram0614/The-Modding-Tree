5af4be36dab253c3f23c7e65915c6be06dc3e5e0
haram0614 <139355979+haram0614@users.noreply.github.com>
2024-09-06 15:42:47 +0900

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -125,6 +125,20 @@ addLayer("q", {
                 return player.points.log10() + "xQt"
             }
         },
+	23: {
+            title: "8",
+            description: "quantum boost PL after Upgrade3 cap",
+            cost: new Decimal(1e1290),
+            unlocked() {
+		    return hasUpgrade("q", 15)
+	    },
+              effect(){
+                return true
+            },
+            effectDisplay() {
+                return player.q.points.add(1e308).log10().mul(2).add(0.25).pow(0.5).pow_base(1.165).mul(169000) + "xPL"
+            },
+        },
     },
     layerShown(){return true}
 })
