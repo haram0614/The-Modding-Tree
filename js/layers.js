29b2404d787f88b308c27206b8742012b25bc85d
haram0614 <139355979+haram0614@users.noreply.github.com>
2024-09-16 15:20:26 +0900

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -323,3 +323,34 @@ addLayer("Qc", {
     },
     layerShown(){return true}
 })
+addLayer("T", {
+    name: "Theory", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "T", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: true,
+		points: new Decimal(0),
+    }},
+    color: "#FFA500",
+    requires: new Decimal("1e6160"), // Can be a function that takes requirement increases into account
+    resource: "Theory", // Name of prestige currency
+    baseResource: "Qt", // Name of resource prestige is based on
+    baseAmount() {return player.q.points}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 0, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = player.q.points.mul("1e6160").log10().div(6161).log10().mul(3.32)
+        mult = player.mult.mul(mult.add(1).div(2))
+        mult = mult.sub(player.t.points)
+        mult = mult.max(0)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    row: 0, // Row the layer is in on the tree (0 is the first row)
+    hotkeys: [
+        {key: "t", description: "T: Reset for theory", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+    layerShown(){return true}
+})
