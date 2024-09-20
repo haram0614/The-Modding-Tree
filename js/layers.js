de7ee0294d5c070260a8fb10f670939605b39cda
haram0614 <139355979+haram0614@users.noreply.github.com>
2024-09-20 15:13:11 +0900

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -342,7 +342,7 @@ addLayer("T", {
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = player.q.points.mul("1e6160").log10().div(6161).log10().mul(3.32)
         mult = player.mult.mul(mult.add(1).div(2))
-        if player.t.points.gte(Decimal(1)) mult = mult.sub(player.t.points)
+        mult = mult.sub(player.t.points)
         mult = mult.max(0)
         return mult
     },
