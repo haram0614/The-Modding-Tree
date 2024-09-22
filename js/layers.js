5ae2d09f87ac6682ab82d902fdf5a7b3fbe1f3c6
haram0614 <139355979+haram0614@users.noreply.github.com>
2024-09-23 08:28:22 +0900

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -337,7 +337,7 @@ addLayer("Qc", {
     },
     layerShown(){return true}
 })
-addLayer("T", {
+addLayer("t", {
     name: "Theory", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "T", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 1, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
