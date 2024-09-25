efe395aa3978aff2491f568e55984ad267065916
haram0614 <139355979+haram0614@users.noreply.github.com>
2024-09-26 08:37:31 +0900

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -389,7 +389,7 @@ addLayer("t", {
                 return true
             },
              effectDisplay() {
-				return "^" "player[layer].resetTime.add(10).log10()"
+				return "^" + "player[layer].resetTime.add(10).log10()"
             }
 	},
         12: {
