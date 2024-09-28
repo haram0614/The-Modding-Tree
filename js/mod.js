2695c693aad142738572602af301dcc5ab19d774
haram0614 <139355979+haram0614@users.noreply.github.com>
2024-09-28 11:43:15 +0900

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -58,7 +58,7 @@ var displayThings = [
 
 // Determines when the game "ends"
 function isEndgame() {
-	return player.q.points.gte(new Decimal("e1e7"))
+	return player.q.points.gte(new Decimal("e5e8"))
 }
 
 
