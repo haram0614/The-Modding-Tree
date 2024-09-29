781c360ce1e0330565cd6c3bbd6210f67bef8411
haram0614 <139355979+haram0614@users.noreply.github.com>
2024-09-29 22:22:31 +0900

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -569,7 +569,7 @@ addLayer("a", {
     exponent: 1, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         multb = new Decimal(0)
-	if (hasUpgrade("q", 33) || hasUpgrade("t",13) multb = multb.add(1)
+	if (hasUpgrade("q", 33) || hasUpgrade("t",13)) multb = multb.add(1)
         return multb
     },
     gainExp() { // Calculate the exponent on main currency from bonuses
