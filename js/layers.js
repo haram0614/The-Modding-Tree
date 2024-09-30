96ddbc53bf13932f4575681d987abb99e0cc214e
haram0614 <139355979+haram0614@users.noreply.github.com>
2024-10-01 00:05:01 +0900

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -365,6 +365,7 @@ addLayer("Qc", {
     branches: ["q"],
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
+	    if (hasUpgrade('Wa', 12)) mult = mult.mul(upgradeEffect('Wa',12))
         return mult
     },
     gainExp() { // Calculate the exponent on main currency from bonuses
@@ -735,6 +736,86 @@ update(diff) {
             },
              effectDisplay() {
 				return upgradeEffect('W',13) + "x waves"
+            }
+	},
+    },
+    layerShown(){return hasUpgrade("a", 13)}
+})
+addLayer("Wa", {
+    name: "Wave2", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "W2", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 3, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: true,
+		points: new Decimal(0),
+    }},
+    color: "#989898",
+    requires: new Decimal(24), // Can be a function that takes requirement increases into account
+    resource: "Waves", // Name of prestige currency
+    baseResource: "Theory", // Name of resource prestige is based on
+    baseAmount() {return player.t.points}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 1, // Prestige currency exponent
+    branches: ["W"],
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        multw = new Decimal(0)
+	if (hasUpgrade("a", 13)) multw = multw.add(1)
+	if (hasUpgrade("Wa", 13)) multw = multw.mul(upgradeEffect('W',13))
+        return multw
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    row: 1, // Row the layer is in on the tree (0 is the first row)
+    hotkeys: [
+        {key: "v", description: "V: Reset for Wave2", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+update(diff) {
+	if (hasUpgrade('W', 11)) generatePoints('W',diff);
+},
+	 upgrades: {
+        rows: 5,
+        cols: 5,
+        11: {
+            title: "51",
+            description: "auto gain waves",
+            
+            cost: new Decimal(1),
+            effect(){
+                return true
+            },
+             effectDisplay() {
+				return "nice"
+            }
+	},
+        12: {
+            title: "52",
+            description: "Boost Qc base",
+            
+            cost: new Decimal(100),
+            unlocked() {
+		    return true
+	    },
+            effect(){
+                return player.Wa.points
+            },
+             effectDisplay() {
+				return upgradeEffect('Wa',12) + "x Qc base"
+            }
+	},
+        13: {
+            title: "43",
+            description: "Qt boost waves",
+            
+            cost: new Decimal(400),
+            unlocked() {
+		    return true
+	    },
+            effect(){
+                return player.q.points.add("e9e15").log10().div(500).log10()
+            },
+             effectDisplay() {
+				return upgradeEffect('Wa',13) + "x waves"
             }
 	},
     },
