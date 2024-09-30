c03243c2e6ed7abcbd4b47e7669475a8fed1df75
haram0614 <139355979+haram0614@users.noreply.github.com>
2024-09-30 15:51:34 +0900

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -630,3 +630,32 @@ addLayer("a", {
     },
     layerShown(){return hasUpgrade("q", 33) || hasUpgrade("t",13)}
 })
+addLayer("W", {
+    name: "Waves", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "W", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 2, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: true,
+		points: new Decimal(0),
+    }},
+    color: "#4BDC13",
+    requires: new Decimal(8), // Can be a function that takes requirement increases into account
+    resource: "prestige points", // Name of prestige currency
+    baseResource: "points", // Name of resource prestige is based on
+    baseAmount() {return player.points}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 1, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        multw = new Decimal(0)
+	if (hasUpgrade("a", 13)) multw = multw.add(1)
+        return multw
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    row: 0, // Row the layer is in on the tree (0 is the first row)
+    hotkeys: [
+        {key: "w", description: "W: Reset for Waves", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+    layerShown(){return hasUpgrade("a", 13)}
+})
