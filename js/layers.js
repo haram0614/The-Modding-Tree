d1c18df5d6902fd1263edb175a65eb38bfe1bef3
haram0614 <139355979+haram0614@users.noreply.github.com>
2024-10-01 14:14:57 +0900

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -693,7 +693,7 @@ addLayer("a", {
 addLayer("W", {
     name: "Waves", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "W", // This appears on the layer's node. Default is the id with the first letter capitalized
-    position: 2, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    position: 3, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
 		points: new Decimal(0),
@@ -865,6 +865,35 @@ update(diff) {
     },
     layerShown(){return hasUpgrade("a", 13)}
 })
+addLayer("SL", {
+    name: "Super PL", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "SL", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 2, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: true,
+		points: new Decimal(0),
+    }},
+    color: "#FF00FF",
+    requires: new Decimal(10), // Can be a function that takes requirement increases into account
+    resource: "Super Quantum", // Name of prestige currency
+    baseResource: "points", // Name of resource prestige is based on
+    baseAmount() {return player.points}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 0, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new Decimal(1.8e43)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    row: 1, // Row the layer is in on the tree (0 is the first row)
+    hotkeys: [
+        {key: "L", description: "L: Reset for Super PL", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+    layerShown(){return hasUpgrade("a",14)}
+})
+
 addLayer("S", {
     name: "Super Quantum", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "S", // This appears on the layer's node. Default is the id with the first letter capitalized
@@ -887,7 +916,7 @@ addLayer("S", {
     gainExp() { // Calculate the exponent on main currency from bonuses
         return new Decimal(1)
     },
-    row: 1, // Row the layer is in on the tree (0 is the first row)
+    row: 0, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
         {key: "S", description: "S: Reset for Super Quantum", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
