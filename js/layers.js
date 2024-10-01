0f0fc4634e0fd2cf697fa8fe4eeb034647225eaf
haram0614 <139355979+haram0614@users.noreply.github.com>
2024-10-01 12:41:42 +0900

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -873,7 +873,7 @@ addLayer("S", {
         unlocked: true,
 		points: new Decimal(0),
     }},
-    color: "#4BDC13",
+    color: "#00FFFF",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
     resource: "Super Quantum", // Name of prestige currency
     baseResource: "points", // Name of resource prestige is based on
@@ -887,7 +887,7 @@ addLayer("S", {
     gainExp() { // Calculate the exponent on main currency from bonuses
         return new Decimal(1)
     },
-    row: 0, // Row the layer is in on the tree (0 is the first row)
+    row: 1, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
         {key: "S", description: "S: Reset for Super Quantum", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
