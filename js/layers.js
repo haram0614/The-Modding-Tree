0b8332d95cf8941d910fb1c9a6d1e4d972e3b68e
haram0614 <139355979+haram0614@users.noreply.github.com>
2024-10-28 18:43:08 +0900

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1319,3 +1319,32 @@ update(diff) {
     },
     layerShown(){return true}
 })
+addLayer("I", {
+    name: "Infinity point", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "I", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 1, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: true,
+		points: new Decimal(0),
+    }},
+    color: "#C68A2C",
+    requires: new Decimal(308.3), // Can be a function that takes requirement increases into account
+    resource: "IP", // Name of prestige currency
+    baseResource: "Qt expo^2", // Name of resource prestige is based on
+    baseAmount() {return player.q.points.log10().log10()}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 1, // Prestige currency exponent
+    branches: ["Qc","Y","Wa","S"],
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    row: 2, // Row the layer is in on the tree (0 is the first row)
+    hotkeys: [
+        {key: "i", description: "I: Reset for IP", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+    layerShown(){return true}
+})
