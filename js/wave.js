ba63af97a89568a6b8f1254f4f609e4c116d9efb
haram0614 <139355979+haram0614@users.noreply.github.com>
2024-10-30 17:20:55 +0900

    Update wave.js

--- js/wave.js
+++ js/wave.js
@@ -347,3 +347,60 @@ update(diff) {
     },
     layerShown(){return hasUpgrade("a", 13)}
 })
+addLayer("Qk", {
+    name: "Quark", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "Qk", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 3, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: true,
+		points: new Decimal(0),
+    }},
+    color: "#00FF7F",
+    requires: new Decimal(1e80), // Can be a function that takes requirement increases into account
+    resource: "Quark", // Name of prestige currency
+    baseResource: "Waves", // Name of resource prestige is based on
+    baseAmount() {return player.W.points.add(player.Qk.points.mul(1e80)}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 0, // Prestige currency exponent
+    branches: ["Wa","Wb"],
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        multw = new Decimal(1)
+        return multw
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    row: 2, // Row the layer is in on the tree (0 is the first row)
+update(diff) {
+	if (player.Qk.points.gte(1)) generatePoints('Qk',diff);
+},
+	 upgrades: {
+        rows: 5,
+        cols: 5,
+        11: {
+            title: "131",
+            description: "Qk boost SP",
+            
+            cost: new Decimal(1),
+            effect(){
+                return player.Qk.points.pow(18)
+            },
+             effectDisplay() {
+				return 
+            }
+	},
+        12: {
+            title: "132",
+            description: "Reduce 2th Spreon Softcap",
+            
+            cost: new Decimal(1),
+            effect(){
+                return true
+            },
+             effectDisplay() {
+				return 
+            }
+	},
+    },
+    layerShown(){return hasUpgrade("a", 13)}
+})
