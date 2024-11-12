9a088e116b79ba4b26f1c47ff460a3ddf66205bf
haram0614 <139355979+haram0614@users.noreply.github.com>
2024-11-13 08:49:26 +0900

    Update wave.js

--- js/wave.js
+++ js/wave.js
@@ -491,3 +491,52 @@ addLayer("e", {
     },
     layerShown(){return hasUpgrade("Wa",15)}
 })
+addLayer("SQ", {
+    name: "SQuark", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "SQ", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 2, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: true,
+		points: new Decimal(0),
+    }},
+    color: "#00FF7F",
+    requires: new Decimal("1.8e308"), // Can be a function that takes requirement increases into account
+    resource: "SQuark", // Name of prestige currency
+    baseResource: "Spreon", // Name of resource prestige is based on
+    baseAmount() {return player.SP.points}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 0, // Prestige currency exponent
+    branches: ["W4"],
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        multw = new Decimal(1)
+	if (true) multw = multw.mul(player.SP.points.add(1).log10().div(308.26).pow(0.1875).sub(1).pow_base(10))
+        return multw
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        exp = new Decimal(1)
+	return exp
+    },
+    row: 3, // Row the layer is in on the tree (0 is the first row)
+update(diff) {
+	if (hasUpgrade('SQ',11)) generatePoints('SQ',diff);
+},
+	 upgrades: {
+        rows: 5,
+        cols: 5,
+        11: {
+            title: "131",
+            description: "Qk boost SP also Auto Quark gain",
+            
+            cost: new Decimal(1),
+            effect(){
+		base = new Decimal(9)
+		if (hasAchievement("A",32)) base = base.add(41)
+                return player.Qk.points.add(1).pow(base)
+            },
+             effectDisplay() {
+				return upgradeEffect('Qk',11)
+            }
+	},
+    },
+    layerShown(){return hasUpgrade("W", 15) || hasUpgrade('Qk',11)}
+})
