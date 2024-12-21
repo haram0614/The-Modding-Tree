5b17c355bf824ccddc9ff4ae1a30f0182a6a0d7e
haram0614 <139355979+haram0614@users.noreply.github.com>
2024-12-21 12:25:50 +0900

    Update h.js

--- js/h.js
+++ js/h.js
@@ -14,12 +14,12 @@ addLayer("p", {
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
-        mult = new Decimal(math.random().mul(100000))
+        mult = new Decimal(math.random().mul(1000))
         return mult
     },
     gainExp() { // Calculate the exponent on main currency from bonuses
         return new Decimal(1)
     },
-    row: 5, // Row the layer is in on the tree (0 is the first row)
+    row: 0, // Row the layer is in on the tree (0 is the first row)
     layerShown(){return true}
 })
