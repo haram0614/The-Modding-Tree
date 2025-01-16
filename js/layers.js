125b00b71f4a8e4c5cb095f879e5a03bd6b4226c
haram0614 <139355979+haram0614@users.noreply.github.com>
2025-01-16 22:41:56 +0900

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -2102,6 +2102,7 @@ update(diff) {
 	         if (hasUpgrade('f', 15)) base = base.add(0.5)
 	         if (hasUpgrade('SL',11)) base = base.add(2.2).add(upgradeEffect('q',24).div(5))
 	         if (hasUpgrade('I', 22)) base = base.max(10)
+	         if (hasUpgrade('SL',11) && hasUpgrade('a',25)) base.max(upgradeEffect('q',24).div(5).pow(2))
 	         if (hasUpgrade('q', 11)) exp = exp.add(1)
 	         if (hasUpgrade('q', 12)) exp = exp.add(1)
 	         if (hasUpgrade('q', 14)) exp = exp.add(1)
