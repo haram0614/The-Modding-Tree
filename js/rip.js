dd8a490eb9054456ffc3b93e492eaacf8bb68fe2
haram0614 <139355979+haram0614@users.noreply.github.com>
2025-01-16 23:22:20 +0900

    Update rip.js

--- js/rip.js
+++ js/rip.js
@@ -1,7 +1,7 @@
 addLayer("rq", {
     name: "ripped quantum", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "rQ", // This appears on the layer's node. Default is the id with the first letter capitalized
-    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    position: 4, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
 		points: new Decimal(0),
@@ -14,14 +14,14 @@ addLayer("rq", {
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
-        mult = new Decimal(1)
+        mult = player.points.add("ee100").log10().log10().pow(0.5).pow_base(10)
         return mult
     },
     gainExp() { // Calculate the exponent on main currency from bonuses
         return new Decimal(1)
     },
     row: 0, // Row the layer is in on the tree (0 is the first row)
-    layerShown(){return false}
+    layerShown(){return hasUpgrade("e",14)}
 })
 addLayer("rQc", {
     name: "Ripped Qc", // This is optional, only used in a few places, If absent it just uses the layer id.
@@ -33,9 +33,9 @@ addLayer("rQc", {
     }},
     color: "#4BDC13",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
-    resource: "prestige points", // Name of prestige currency
-    baseResource: "points", // Name of resource prestige is based on
-    baseAmount() {return player.points}, // Get the current amount of baseResource
+    resource: "Ripped Qc", // Name of prestige currency
+    baseResource: "rQc", // Name of resource prestige is based on
+    baseAmount() {return player.rq.points.add(1).log10()}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
@@ -46,5 +46,5 @@ addLayer("rQc", {
         return new Decimal(1)
     },
     row: 1, // Row the layer is in on the tree (0 is the first row)
-    layerShown(){return false}
+    layerShown(){return hasUpgrade("a",14)}
 })
