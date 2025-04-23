26e5a43cf0cc36e2c2be3bfba8cf0fe527caa040
haram0614 <139355979+haram0614@users.noreply.github.com>
2025-04-23 12:47:09 +0900

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -576,10 +576,10 @@ addLayer("Qc", {
     baseResource: "Qt expo", // Name of resource prestige is based on
     baseAmount() {return player.q.points.add(1).log10()}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    exponent: 1, // Prestige currency exponent
+    exponent: 0, // Prestige currency exponent
     branches: ["q"],
     gainMult() { // Calculate the multiplier for main currency from bonuses
-        mult = new Decimal(1)
+        mult = player.q.points.add(1e43).log10()
         exp = new Decimal(1)
 	one = new Decimal(1)
 	    if (hasUpgrade('q', 21)) exp = exp.add(1)
